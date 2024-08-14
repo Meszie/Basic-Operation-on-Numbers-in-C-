590ab2ff5550eb5331284520a8baeaf015a3d2ca
Meszie <aberin.marlyn011@gmail.com>
2024-08-14 11:35:14 +0800

    Create index.cs
    Write code to perform addition, subtraction, multiplication, and division with integers

---
+++ index.cs
@@ -0,0 +1,10 @@
+// Perform basic math operations
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum); //value Sum: 12
+Console.WriteLine("Difference: " + difference); //value Difference: 2
+Console.WriteLine("Product: " + product); //value Product: 35
+Console.WriteLine("Quotient: " + quotient); //value Quotient: 1
