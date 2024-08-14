076fab54a5a4edb2fdf41f0ab1a10a4727fb8a6c
Meszie <aberin.marlyn011@gmail.com>
2024-08-14 11:53:10 +0800

    Create increment-decrement.net

---
+++ increment-decrement.cs
@@ -0,0 +1,22 @@
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);//output Third increment: 4
+
+value += 1;
+Console.WriteLine("Second increment: " + value);//output Second increment: 3
+
+value++;
+Console.WriteLine("Third increment: " + value);//output First increment: 2
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);//output First decrement: 3
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);//output Second decrement: 2
+
+value--;
+Console.WriteLine("Third decrement: " + value);//output Third decrement: 1
+
+
+
